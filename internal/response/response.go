020b70d9972ecf11c4d09d551c39aea18c36910e
Ioannis Tsiros <jtsiros93@gmail.com>
2026-01-03 17:58:32 +0100

    Server is working! Trying to find a nice way to implement error codes and responces plus adding handlers

---
+++ internal/response/response.go
@@ -0,0 +1,105 @@
+package response
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"strconv"
+
+	"github.com/tsironi93/miniHttp/internal/headers"
+)
+
+const ContLen = "Content-Length"
+const Conn = "Connection"
+const ContType = "Content-Type"
+
+type StatusCode int
+
+const (
+	SC200 = iota
+	SC400
+	SC500
+)
+
+type HandleError struct {
+	Code StatusCode
+	Msg  string
+}
+
+func WriteErrorResponse(w io.Writer, code StatusCode, msg string) {
+	body := msg + "\n"
+
+	h := GetDefaultHeaders(len(body))
+
+	WriteStatusLine(w, code)
+	WriteHeaders(w, h)
+	io.WriteString(w, body)
+}
+
+func WriteError(w io.Writer, err error) {
+	var errHandler *HandleError
+
+	if errors.As(err, &errHandler) {
+		WriteErrorResponse(w, errHandler.Code, errHandler.Msg)
+		return
+	}
+}
+
+func WriteHeaders(w io.Writer, h headers.Headers) error {
+	s := fmt.Sprintf(
+		"%s: %s\r\n%s: %s\r\n%s: %s\r\n\r\n",
+		ContLen, h[ContLen],
+		Conn, h[Conn],
+		ContType, h[ContType],
+	)
+
+	_, err := io.WriteString(w, s)
+	return err
+}
+
+func GetDefaultHeaders(contentLen int) headers.Headers {
+	h := headers.NewHeaders()
+	h[ContLen] = strconv.Itoa(contentLen)
+	h[Conn] = "close"
+	h[ContType] = "text/plain"
+
+	return h
+}
+
+func WriteStatusLine(w io.Writer, statusCode StatusCode) error {
+	if _, err := io.WriteString(w, "HTTP/1.1 "); err != nil {
+		return err
+	}
+
+	switch statusCode {
+	case SC200:
+		_, err := io.WriteString(w, "200 OK\r\n")
+		return err
+	case SC400:
+		_, err := io.WriteString(w, "400 Bad Request\r\n")
+		return err
+	case SC500:
+		_, err := io.WriteString(w, "500 Internal Server Error\r\n")
+		return err
+	default:
+		return nil
+	}
+}
+
+func (e *HandleError) Error() string {
+	return e.Msg
+}
+
+func BadRequest(msg string) *HandleError {
+	return &HandleError{
+		Code: SC400,
+		Msg:  msg,
+	}
+}
+
+func InternalError(msg string) *HandleError {
+	return &HandleError{
+		Code: SC500,
+		Msg:  msg,
+	}
+}
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,5 +1,78 @@
 package server
 
-func (s Server) Serve(port int) {
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"sync/atomic"
 
+	"github.com/tsironi93/miniHttp/internal/request"
+	"github.com/tsironi93/miniHttp/internal/response"
+)
+
+type Server struct {
+	listener net.Listener
+	closed   atomic.Bool
+}
+
+type HandlerFunc func(w io.Writer, req *request.Request) *response.HandleError
+
+func (s *Server) Close() error {
+	s.closed.Store(true)
+	return s.listener.Close()
+}
+
+func (s *Server) handleRequest(conn net.Conn) (*request.Request, error) {
+
+	r, err := request.RequestFromReader(conn)
+	if err != nil {
+		return nil, err
+	}
+
+	return r, nil
+}
+
+func (s *Server) handle(conn net.Conn) {
+	defer conn.Close()
+
+	if err := s.handleRequest(conn); err != nil {
+		response.WriteError(conn, err)
+		return
+	}
+
+	h := response.GetDefaultHeaders(0)
+	response.WriteStatusLine(conn, response.SC200)
+	response.WriteHeaders(conn, h)
+
+}
+
+func (s *Server) listen() {
+	for {
+		conn, err := s.listener.Accept()
+		if err != nil {
+			log.Println(err)
+			if s.closed.Load() {
+				return
+			}
+		}
+		go s.handle(conn)
+	}
+}
+
+func Serve(port int, handler *HandlerFunc) (*Server, error) {
+
+	ln, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	s := &Server{
+		listener: ln,
+	}
+
+	go s.listen()
+
+	return s, nil
 }
